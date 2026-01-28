df0b86cf73fe79b2a314c17215de9d67de4f03f3
rajpratap29 <therajpratapofficial@gmail.com>
2026-01-29 00:15:02 +0530

    Update: Added sidebar and project versions

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -14,7 +14,8 @@ import {
   TabletIcon,
   XIcon,
 } from "lucide-react";
-import { dummyConversations, dummyProjects } from "../assets/assets";
+import { dummyConversations, dummyProjects, dummyVersion } from "../assets/assets";
+import Sidebar from "../components/Sidebar";
 
 const Projects = () => {
   const { projectId } = useParams();
@@ -35,7 +36,7 @@ const Projects = () => {
     const project = dummyProjects.find((project) => project.id === projectId);
     setTimeout(() => {
       if (project) {
-        setProject({ ...project, conversation: dummyConversations });
+        setProject({ ...project, conversation: dummyConversations, versions: dummyVersion });
         setLoading(false);
         setIsGenerating(project.current_code ? false : true);
       }
@@ -158,10 +159,14 @@ const Projects = () => {
         </div>
       </div>
       <div className="flex-1 flex overflow-auto">
-            <div>Sidebar</div>
-            <div className="flex-1 p-2 pl-0">
-              Project Preview
-            </div>
+        <Sidebar
+          isMenuOpen={isMenuOpen}
+          project={project}
+          setProject={(p) => setProject(p)}
+          isGenerating={isGenerating}
+          setIsGenerating={setIsGenerating}
+        />
+        <div className="flex-1 p-2 pl-0">Project Preview</div>
       </div>
     </div>
   ) : (
