16261103795d3e36ac3c1b6d42a724db646f2bd6
rajpratap29 <therajpratapofficial@gmail.com>
2026-02-04 00:06:16 +0530

    Update: Added fetching of published projects from database

--- client/src/pages/View.tsx
+++ client/src/pages/View.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { dummyProjects } from "../assets/assets";
 import { Loader2Icon } from "lucide-react";
 import ProjectPreview from "../components/ProjectPreview";
 import type { Project } from "../types";
+import { toast } from "sonner";
+import api from "@/configs/axios";
 
 const View = () => {
   const { projectId } = useParams();
@@ -11,15 +12,14 @@ const View = () => {
   const [loading, setLoading] = useState(true);
 
   const fetchCode = async () => {
-    const code = dummyProjects.find(
-      (project) => project.id === projectId,
-    )?.current_code;
-    setTimeout(() => {
-      if (code) {
-        setCode(code);
-        setLoading(false);
-      }
-    }, 2000);
+    try {
+      const { data } = await api.get(`api/project/published/${projectId}`);
+      setCode(data.code);
+      setLoading(false);
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message || error.message);
+      console.log(error);
+    }
   };
 
   useEffect(() => {
