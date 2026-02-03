158392ce8b913db30c9ac00bb43cbef5ad8c9bb8
rajpratap29 <therajpratapofficial@gmail.com>
2026-02-03 23:36:59 +0530

    Update: Added project deletion functionality

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -1,28 +1,54 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { Project } from "../types";
 import { Loader2Icon, PlusIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import { dummyProjects } from "../assets/assets";
 import Footer from "../components/Footer";
+import api from "@/configs/axios";
+import { toast } from "sonner";
+import { authClient } from "@/lib/auth-client";
 
 const MyProjects = () => {
+  const { data: session, isPending } = authClient.useSession();
   const [loading, setLoading] = useState(true);
   const [projects, setProjects] = useState<Project[]>([]);
   const navigate = useNavigate();
 
   const fetchProjects = async () => {
-    setProjects(dummyProjects);
-    // simulate loading
-    setTimeout(() => {
+    try {
+      const { data } = await api.get(`/api/user/projects`);
+      setProjects(data.projects);
       setLoading(false);
-    }, 1000);
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message || error.message);
+      console.log(error);
+    }
   };
 
-  const deleteProject = async (projectId: string) => {};
+  const deleteProject = async (projectId: string) => {
+    try {
+      const confirm = window.confirm(
+        "Are you sure you want to delete this project?",
+      );
+      if (!confirm) {
+        return;
+      }
+      const { data } = await api.delete(`/api/project/${projectId}`);
+      toast.success(data.message);
+      fetchProjects();
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message || error.message);
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    fetchProjects();
-  }, []);
+    if (session?.user && !isPending) {
+      fetchProjects();
+    } else if (!isPending && !session?.user) {
+      navigate("/");
+      toast("Please login to view your projects");
+    }
+  }, [session?.user]);
   return (
     <>
       <div className="px-4 md:px-16 lg:px-24 xl:px-32">
@@ -56,7 +82,7 @@ const MyProjects = () => {
                     {project.current_code ? (
                       <iframe
                         srcDoc={project.current_code}
-                        className="absolute top-0 left-0 w-[1200px] h-[800px] origin-top-left pointer-events-none"
+                        className="absolute top-0 left-0 w-300 h-200 origin-top-left pointer-events-none"
                         sandbox="allow-scripts allow-same-origin"
                         style={{ transform: "scale(0.25)" }}
                       />
