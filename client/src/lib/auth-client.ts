526ee83fca2acefe23d90f74403276bdda310e88
rajpratap29 <therajpratapofficial@gmail.com>
2026-02-05 00:00:17 +0530

    Update: fix error for frontend base url

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,7 +1,11 @@
 import { createAuthClient } from "better-auth/react";
+const baseUrl = import.meta.env.VITE_BASEURL;
 export const authClient = createAuthClient({
-  baseURL: "/api",
+  baseURL: baseUrl.startsWith("/")
+    ? `${window.location.origin}${baseUrl}`
+    : baseUrl,
   fetchOptions: { credentials: "include" },
 });
 
 export const { signIn, signUp, useSession } = authClient;
+
